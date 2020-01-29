96238d41c00e1fd981ad755a4fcbd7213ea51b5c
Andrew DiCarlo <andrew.dicarlo@gmail.com>
2020-01-28 19:42:07 -0600

    Round one of function test

--- Lab1/AndrewDiCarloLab1methods.c
+++ Lab1/AndrewDiCarloLab1methods.c
@@ -1,7 +1,18 @@
 #include "Lab1.h"
+#include <stdio.h>
 
 void displayBits(unsigned int n)
 {
+    int maskLen = (8 * sizeof(n)) - 1;
+    unsigned int mask;
+
+    for (mask = 1 << maskLen; mask > 0; mask = mask >> 1) {
+        printf("%d", mask & n);
+        if (!(mask % 8)) {
+            printf(" ");
+        }
+    }
+    printf("\n");
 }
 unsigned int setKthBit(unsigned int n, int k);
 int isPowerOfTwo(unsigned int n);
