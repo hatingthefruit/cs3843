b40329930226406d1d176ac51f5e425ec36d06b0
Andrew DiCarlo <andrew.dicarlo@gmail.com>
2020-01-28 19:30:27 -0600

    Created a header for our functions, copied definitions over to the methods file. Autoformat changed most of the main file

---
+++ Lab1/AndrewDiCarloLab1methods.c
@@ -0,0 +1,9 @@
+#include "Lab1.h"
+
+void displayBits(unsigned int n)
+{
+}
+unsigned int setKthBit(unsigned int n, int k);
+int isPowerOfTwo(unsigned int n);
+int getNoBits(unsigned int a, unsigned int b);
+unsigned int computeXor(unsigned int n);
