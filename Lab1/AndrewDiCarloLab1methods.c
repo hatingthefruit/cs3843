0665b37f419173c25ef4e152a9708c26daad3580
Andrew DiCarlo <andrew.dicarlo@gmail.com>
2020-01-29 17:06:51 -0600

    Misc makefile changes (added a clean), changed all functions so they can be run without errors, final version of displayBits

--- Lab1/AndrewDiCarloLab1methods.c
+++ Lab1/AndrewDiCarloLab1methods.c
@@ -5,16 +5,34 @@ void displayBits(unsigned int n)
 {
     int maskLen = (8 * sizeof(n)) - 1;
     unsigned int mask;
+    int count = 0;
+
+    printf("      %d =", n); // 6 spaces in order to match up with the provided output file
+                             // No space after the = because the loop will print a space out the first iteration
 
     for (mask = 1 << maskLen; mask > 0; mask = mask >> 1) {
-        printf("%d", mask & n);
-        if (!(mask % 8)) { // Prints out spaces every time, needs to be changed
+        if (!(count % 8)) { // Only print a space if the count is divisible by 8
             printf(" ");
         }
+        count++;
+
+        printf("%d", mask & n);
     }
     printf("\n");
 }
-unsigned int setKthBit(unsigned int n, int k);
-int isPowerOfTwo(unsigned int n);
-int getNoBits(unsigned int a, unsigned int b);
-unsigned int computeXor(unsigned int n);
+unsigned int setKthBit(unsigned int n, int k)
+{
+    return k;
+}
+int isPowerOfTwo(unsigned int n)
+{
+    return n;
+}
+int getNoBits(unsigned int a, unsigned int b)
+{
+    return 32;
+}
+unsigned int computeXor(unsigned int n)
+{
+    return n;
+}
